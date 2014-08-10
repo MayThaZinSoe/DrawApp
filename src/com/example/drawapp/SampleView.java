2eedcb141a9bec25027d9807df288fb46ec706fe
MayThaZinSoe <tzsakura@gmail.com>
2014-08-10 16:25:43 +0900

    change color red to blue and blue to red if click

--- src/com/example/drawapp/SampleView.java
+++ src/com/example/drawapp/SampleView.java
@@ -10,6 +10,11 @@ import android.widget.Toast;
 
 public class SampleView extends View {
 
+	private Paint paint = new Paint();
+	private int color= Color.RED ;
+	private int bx = 100;
+	private int by = 100;
+	
 	/**
 	 * @param args
 	 */
@@ -17,22 +22,44 @@ public class SampleView extends View {
 	
 	public SampleView(Context context){
 	super(context);
-	setBackgroundColor(Color.GREEN);
+	setBackgroundColor(Color.TRANSPARENT);
 	
 	}
 	@Override
 	public void onDraw(Canvas canvas){
-		paint.setColor(Color.RED);
-		canvas.drawCircle( 200, 200,20,paint);
+		paint.setColor(color);
+		canvas.drawCircle( 100, 100,20,paint);
 	
 	}
-	private Paint paint = new Paint();
+	
+	
 	
 	
 	@Override
 	public boolean onTouchEvent(MotionEvent event){
-		Toast.makeText(getContext(),"Change Touch",Toast.LENGTH_SHORT).show();
 		
+		
+		int action = event.getAction();
+		if((action & MotionEvent.ACTION_MASK)==
+				MotionEvent.ACTION_DOWN){
+			int ex = (int)event.getX();
+			int ey = (int)event.getY();
+		
+			if ((bx - ex)*(bx - ex)+(by  - ey)*(by - ey) <= 20*20) {    
+			Toast.makeText(getContext(),"Change Touch",Toast.LENGTH_SHORT).show();
+			
+			//change color,if blue,change to red
+			if(color == Color.BLUE){
+				color = Color.RED;
+			}else{
+				color = Color.BLUE;
+			}
+			
+			invalidate();//repeate
+			}
+		
+		
+		}
 		
 		return true;
 	}
