97c8ca20902efb3645423270fcb56875e47ffe8c
MayThaZinSoe <tzsakura@gmail.com>
2014-08-10 14:40:44 +0900

    sampleview run

---
+++ src/com/example/drawapp/SampleView.java
@@ -0,0 +1,40 @@
+package com.example.drawapp;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.View;
+
+public class SampleView extends View {
+
+	/**
+	 * @param args
+	 */
+	
+	
+	public SampleView(Context context){
+	super(context);
+	setBackgroundColor(Color.GREEN);
+	
+	}
+	@Override
+	public void onDraw(Canvas canvas){
+		paint.setColor(Color.RED);
+		canvas.drawCircle( 100, 100,5,paint);
+	
+	}
+	private Paint paint = new Paint();
+	
+	
+	
+	
+	
+	
+}
+	
+	
+	
+	
+
+
